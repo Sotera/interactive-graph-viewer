1f1f7851d39349b2b943d62fd9e386ccad5b947b
David Krisiloff <david.krisiloff@soteradefense.com>
2015-11-04 13:02:48 -0500

    Removed debugging output and prevent “zooming” on a single node.

--- server.R
+++ server.R
@@ -12,18 +12,18 @@ function(input, output, session){
   source("external/graph_utils.R", local = TRUE)
   source("external/makenetjson.R", local = TRUE)
 
-  v <- reactiveValues(community = NULL)
+  v <- reactiveValues(community = NULL, current_graph_type = NULL)
   
   # reset button
   observeEvent(input$reset_button, {
     v$community = NULL
-    print(v$community)
   })
   
   # on-click from sigma.js
   observeEvent(input$comm_id, {
-    v$community = input$comm_id
-    print(v$community)
+    if (v$current_graph_type == "community"){
+      v$community = input$comm_id
+    }
   })
   
   # Regenerate the current graph visualization
@@ -31,27 +31,26 @@ function(input, output, session){
     print("-----------")
     # Get the community id
     id <- v$community
-    print(id)
-    
+
     # If we don't have a community then build the first graph,
     # otherwise select the desired community subgraph
     if (is.null(id)){
       graph <<- build_initial_graph(initial_data)
     } else {
-      print("Trying to subgraph")
       graph <<- community_subgraph(graph, communities, id)      
     }
     
     # if the graph we are looking at has more than 200 points 
     # run community detection to make it easier to visualize
-    if (vcount(graph) > 200){
+    if (vcount(graph) > 500){
       communities <<- get_communities(graph)
       V(graph)$comm <-communities$membership
       contracted <- contract.vertices(graph, communities$membership, "random")
       community_graph <- simplify(contracted, "random")
-      makenetjson(community_graph, "./www/data/current_graph.json", comm_graph = TRUE)      
+      makenetjson(community_graph, "./www/data/current_graph.json", comm_graph = TRUE)  
+      v$current_graph_type = "community"
     } else {
-      print("Got here?")
+      v$current_graph_type = "not_community"
       makenetjson(graph, "./www/data/current_graph.json", comm_graph = FALSE)   
     }
     
