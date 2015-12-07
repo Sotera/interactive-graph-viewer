5914660483dfd42beaa6e0b94830b806a55c8529
vinodma <vinod.mangipudi@soteradefense.com>
2015-12-07 14:45:28 -0500

    Update ui.R
    Refactored code to update json via messaging. Also implemented protein search

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 ## ui.R ##
+## ui.R ##
 library(DT)
-library(plotly)
 library(shiny)
 library(shinydashboard)
 
@@ -12,7 +12,7 @@ sidebar <- dashboardSidebar(
   p("Proteins are ",  span("green", style = "color:#77B300")),
   p("Chemicals are ", span("orange", style = "color:#FF8800")), 
   p("Diseases are ", span("red", style = "color:#CC0000")),
-
+  
   actionButton("back_button", "Back"),
   actionButton("reset_button", "Reset"),  
   radioButtons("interactions", "Show interactions:",
@@ -20,36 +20,40 @@ sidebar <- dashboardSidebar(
                  "Protein-Protein" = "Protein-Protein",
                  "Protein-Disease" = "Protein-Disease",
                  "Protein-Chemical" = "Protein-Chemical",
-                 "Chemical-Disease" = "Chemical-Disease"))
+                 "Chemical-Disease" = "Chemical-Disease")),
+  
+  textInput("searchentitiy","Search Protein"),
+  actionButton("search_button","Search")
 )
 
 body <- dashboardBody(
   tags$head(
     tags$script(src='lib/sigma.min.js'),
     tags$script(src='lib/sigma.layout.forceAtlas2.min.js'),
-    tags$script(src='lib/sigma.parsers.json.min.js')
+    tags$script(src='lib/sigma.parsers.json.min.js'),
+    tags$script(src='rendergraph.js'),
+    tags$link(rel = "stylesheet", type = "text/css", href = "graph.css")
   ),
   
   fluidRow(  
     box(     textOutput("name"), 
              uiOutput("graph_with_sigma"),
              title = "Network",
-         header = TRUE,
-         tags$canvas(id="graph", # graphical output area
-                     width="1000",
-                     height="800")
+             header = TRUE,
+             tags$canvas(id="graph", # graphical output area
+                         width="1000",
+                         height="800"),tags$div(id="graph2")
     ),
     
     tabBox( title = "Details", 
-         id = "details",
-         selected = "Entities",
-         tabPanel("Entities", DT::dataTableOutput("degree_table")),
-         tabPanel("Degrees", plotlyOutput("degree_distribution")),
-         tabPanel("PageRanks", plotlyOutput("pagerank_distribution"))
+            id = "details",
+            selected = "Entities",
+            tabPanel("Entities", DT::dataTableOutput("degree_table")),
+            tabPanel("Degrees", plotOutput("degree_distribution")),
+            tabPanel("PageRanks", plotOutput("pagerank_distribution"))
     )
   )
   
 )
 
 dashboardPage(header, sidebar, body)
-
