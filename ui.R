176794f45d3534e13048a5c503695a0e4723bd06
Eli Fajardo <eli.fajardo@soteradefense.com>
2016-10-24 23:54:33 -0400

    Moved things around and a little cleanup.

--- ui.R
+++ ui.R
@@ -4,110 +4,155 @@ library(plotly)
 library(shinydashboard)
 library(colourpicker)
 
-header <- dashboardHeader(title = "Community Visualization v2.0", titleWidth = 450)
+header <- dashboardHeader(
+  title = "Community Visualization v2.0",
+  titleWidth = 450
+)
 
 
 body <- dashboardBody(
   fluidRow(
     column(width=12,
-           tabBox(
-             id='tabvals',
-             width=NULL,
-             tabPanel('Configuration Options',
-                      tabBox(width=500,title="",id="fileinfo",
-                             tabPanel("EdgeListFile",
-                                      fileInput('file1', 'Choose file to upload',
-                                                accept = c(
-                                                  'text/csv',
-                                                  'text/comma-separated-values',
-                                                  'text/tab-separated-values',
-                                                  'text/plain',
-                                                  '.csv',
-                                                  '.tsv'
-                                                )
-                                      ),
-                                      tags$hr(),
-                                      checkboxInput('header', 'Header', TRUE),
-                                      radioButtons('sep', 'Separator',
-                                                   c(Comma=',',
-                                                     Semicolon=';',
-                                                     Tab='\t'),
-                                                   ','),
-                                      radioButtons('quote', 'Quote',
-                                                   c(None='',
-                                                     'Double Quote'='"',
-                                                     'Single Quote'="'"),
-                                                   '"'),
-                                      tags$hr(),
-                                      p('File size limit is 100MB')
-                             ),
-                             tabPanel("Entity definitions",
-                                      selectInput("entity1","Entity1 Column:",c()),
-                                      selectInput("entity2","Entity2 Column:",c()),
-                                      selectInput("type1","Entitiy1 Type Column:",c()),
-                                      selectInput("type2","Entity2 Type Column:",c()),
-                                      actionButton("entitymapping_button", "Done")
-                             ),
-                             tabPanel("Define Entity interactions",
-                                      selectInput("entintr1","Select entity 1",choices=c()),
-                                      selectInput("entintr2","Select entity 2",choices=c()),
-                                      actionButton("entintrdone","Assign interaction"),
-                                      tableOutput("entintrtable")
-                             ),
-                             tabPanel("Entity colors",
-                                      selectInput("entcolors","Select entity",choices=c()),
-                                      colourInput("entcol","Select entity color"),
-                                      actionButton("entdone","Assign color"),
-                                      tableOutput("enttable")
-                                      
-                             ),
-                             tabPanel("Community parameters",
-                                      colourInput("community_col","Community Color","#2ADDDD"),
-                                      numericInput("comm_size","Max. community size:",value = 400)
-                             )
-                             
-                      ),
-                      actionButton("saveoptionscsv","Save"),
-                      mainPanel(
-                        tableOutput('contents')
-                      )
-                      
-                      
-                      
-                      ,value=1),
-             tabPanel('Viewer',
-                      fluidRow(  
-                        box(     textOutput("name"), 
-                                 uiOutput("graph_with_sigma"),
-                                 title = "Network",
-                                 header = TRUE,
-                                 tags$canvas(id="graph", # graphical output area
-                                             width="1000",
-                                             height="800"),tags$div(id="graph2")
-                        ),
-                        
-                        tabBox( title = "Details", 
-                                id = "details",
-                                selected = "Entities",
-                                tabPanel("Entities", DT::dataTableOutput("degree_table")),
-                                tabPanel("Degrees", plotlyOutput("degree_distribution")),
-                                tabPanel("PageRanks", plotlyOutput("pagerank_distribution")),
-                                tabPanel("Disease Pathway Info.", 
-                                         tabBox(width=500,title="",id="pathinfo",
-                                                tabPanel("Data",fluidRow(splitLayout(cellWidths = c("100%", "0%"), DT::dataTableOutput("plotgraph1"))
-                                                ))
-                                                #,tabPanel("Heatmap",plotlyOutput("plotgraph2"))
-                                         )
-                                )
-                                
+      tabBox(
+        id='tabvals',
+        width=NULL,
+        tabPanel(
+          'Viewer',
+          fluidRow (
+
+            column(7,
+              #textOutput("name"),
+              actionButton("reset_button", "Reset"),
+              tags$style(type="text/css", "#reset_button {float: right; margin-left: 5px;}"),
+              actionButton("back_button", "Back"),
+              tags$style(type="text/css", "#back_button {float: right; margin-left: 5px;}"),
+              uiOutput("graph_with_sigma"),
+              #title = "Network",
+              header = TRUE,
+              tags$canvas(
+                id="graph", # graphical output area
+                width="1000",
+                height="800"
+              ),
+              tags$div(id="graph2"),
+              tags$style(type="text/css", "#graph2 {max-width: 1000px; height: 800px; margin: auto;}")
+            ),
+            column(5,
+              wellPanel(
+                selectInput("select",
+                            label = "Select algorithm",
+                            choices = list(
+                              "Louvain" = "lv",
+                              "Walktrap" = "wk",
+                              "Fast Greedy" = "fg",
+                              "Infomap" = "imap",
+                              "Edge betweeness" = "ebetweens",
+                              "Label Propagation"="lp",
+                              "Spinglass"="sg"
+                            ),
+                            selected = "lv"
+                ),
+                # actionButton("back_button", "Back"),
+                # actionButton("reset_button", "Reset"),
+                #hr(),
+                radioButtons("interactions","Show Interactions:",choices=c(0)),
+                textInput("searchentitiy","Search Entity"),
+                actionButton("search_button","Search")
+              ),
+              tabsetPanel(
+                id = "details",
+                selected = "Entities",
+                tabPanel("Entities", DT::dataTableOutput("degree_table")),
+                tabPanel("Degrees", plotlyOutput("degree_distribution")),
+                tabPanel("PageRanks", plotlyOutput("pagerank_distribution")),
+                tabPanel("Disease Pathway", 
+                  tabBox(
+                    width=500,title="",
+                    id="pathinfo",
+                    tabPanel(
+                      "Data",
+                      fluidRow(
+                        splitLayout(
+                          cellWidths = c("100%", "0%"),
+                          DT::dataTableOutput("plotgraph1")
                         )
-                        
                       )
-                      
-                      
-                      
-                      ,value=2)
-           )
+                    )
+                    #,tabPanel("Heatmap",plotlyOutput("plotgraph2"))
+                  )
+                )
+              )
+            )
+
+          ),
+          value=2
+        ),
+        tabPanel(
+          'Configuration Options',
+          tabBox(
+            width=500,
+            title="",
+            id="fileinfo",
+            tabPanel(
+              "EdgeListFile",
+              fileInput(
+                'file1',
+                'Choose file to upload',
+                accept = c(
+                  'text/csv',
+                  'text/comma-separated-values',
+                  'text/tab-separated-values',
+                  'text/plain',
+                  '.csv',
+                  '.tsv'
+                )
+              ),
+              tags$hr(),
+              checkboxInput('header', 'Header', TRUE),
+              radioButtons('sep', 'Separator',
+                c(Comma=',',
+                 Semicolon=';',
+                 Tab='\t'),
+                ','),
+              radioButtons('quote', 'Quote',
+                c(None='',
+                 'Double Quote'='"',
+                 'Single Quote'="'"),
+                '"'),
+              tags$hr(),
+              p('File size limit is 100MB')
+            ),
+            tabPanel("Entity definitions",
+              selectInput("entity1","Entity1 Column:",c()),
+              selectInput("entity2","Entity2 Column:",c()),
+              selectInput("type1","Entitiy1 Type Column:",c()),
+              selectInput("type2","Entity2 Type Column:",c()),
+              actionButton("entitymapping_button", "Done")
+            ),
+            tabPanel("Define Entity interactions",
+              selectInput("entintr1","Select entity 1",choices=c()),
+              selectInput("entintr2","Select entity 2",choices=c()),
+              actionButton("entintrdone","Assign interaction"),
+              tableOutput("entintrtable")
+            ),
+            tabPanel("Entity colors",
+              selectInput("entcolors","Select entity",choices=c()),
+              colourInput("entcol","Select entity color"),
+              actionButton("entdone","Assign color"),
+              tableOutput("enttable")
+            ),
+            tabPanel("Community parameters",
+              colourInput("community_col","Community Color","#2ADDDD"),
+              numericInput("comm_size","Max. community size:",value = 400)
+            )
+          ),
+          actionButton("saveoptionscsv","Save"),
+          mainPanel(
+            tableOutput('contents')
+          ),
+          value=1
+        )
+      )
     ) 
   ),
   tags$script(src='lib/sigma.min.js'),
@@ -120,31 +165,6 @@ body <- dashboardBody(
 
 dashboardPage(
   header,
-  dashboardSidebar(
-    conditionalPanel(
-      condition = "input.tabvals == 1",
-      NULL
-    ),
-    conditionalPanel(
-      condition = "input.tabvals == 2",
-      uiOutput("legend"),
-      selectInput("select", label = h5("Select algorithm"), 
-                  choices = list("Louvain" = "lv", "Walktrap" = "wk", "Fast Greedy" = "fg","Infomap" = "imap","Edge betweeness" = "ebetweens","Label Propagation"="lp","Spinglass"="sg"), 
-                  selected = "lv"),
-      hr(),
-      actionButton("back_button", "Back"),
-      actionButton("reset_button", "Reset"),  
-      radioButtons("interactions","Show Interactions:",choices=c(0)),
-      
-      
-      #fluidRow(column(3, verbatimTextOutput("value"))),
-      # checkboxGroupInput("node_types", "Entities:",
-      #                     choices = c("Protein" , "Disease", "Chemical"),
-      #                     selected = c("Protein" , "Disease", "Chemical")),
-      
-      textInput("searchentitiy","Search Entity"),
-      actionButton("search_button","Search")
-    )
-  ),
+  dashboardSidebar(disable = TRUE),
   body
 )
