a732defba61861b89be6c64cc823ec9ed79fb213
Eli Fajardo <eli.fajardo@soteradefense.com>
2016-11-16 11:22:13 -0500

    added node filtering

--- www/rendergraph.js
+++ www/rendergraph.js
@@ -1,155 +1,133 @@
 $(document).ready(function() {
   
 	var searchelm = "";
-	var filter;
-  
-  sigma.parsers.json("data/current_graph.json",
-  
-    {
-      container: 'graph2'
-    },
 
+  Shiny.addCustomMessageHandler("updategraph",
+    function(message) {
+      console.log("updategraph");
 
-    function(s) { //This function is passed an instance of Sigma s
-      //filter = new sigma.plugins.filter(s);
+      // to delete & refresh the graph
       var g = document.querySelector('#graph2');
-
-      try {
-        Shiny.addCustomMessageHandler("commmemmsg",
-          function(message) {
-            console.log("commmemmsg");
-            JSON.parse(JSON.stringify(message), function(k, v) {
-              if (k == "id") {
-		            elems = String(v).split(",");
-                s.graph.nodes().forEach(
-                  function(node, i, a) {
-                    node.color = node.originalcolor;
-                    for (var ix = 0; ix < elems.length; ix++) {
-                      elem = elems[ix];
-                      if (node.id == elem) {
-                        node.color = "#FFD700";
-                      }
-                    }
-		                searchelm=elems;
-                  }
-                );
-                s.refresh();
-              }
-            });
-          }
-        );
-      }
-      catch (err) {
-      }
-
-      Shiny.addCustomMessageHandler("updategraph",
-        function(message) {
-          console.log("updategraph");
-          // to delete & refresh the graph
-          var g = document.querySelector('#graph2');
-          var p = g.parentNode;
-          p.removeChild(g);
-          var c = document.createElement('div');
-          c.setAttribute('id', 'graph2');
-          p.appendChild(c);
-    
-          //create_graph(); 
-    
-          sigma.parsers.json("data/current_graph.json",
-            {
-              container: 'graph2'
-            },
-            function(new_s) {
-              
-              
-              
-              new_s.graph.nodes().forEach(
-                function(node, i, a) {
-				          if(searchelm!="") {
-  				          for (var ix = 0; ix < searchelm.length; ix++) {
-  					          elem = searchelm[ix];
-            					if (node.id == elem) { 
-                        node.color = "#FFD700";
-            					}
-            					else {
-            						node.color = node.originalcolor;
-            					} 
-                    }
-                  }
-                }
-              );
-              
-              filter = new sigma.plugins.filter(new_s);
-              $(document).on('shiny:inputchanged', function(event) {
-                if (event.name === 'entTypes') {
-                  filter
-                    .undo('test-filter')
-                    .nodesBy(function(n) {
-//                      console.log(n.name);
-//                      console.log(n.type);
-//                      console.log(event.value);
-//                      console.log($.inArray(n.type, event.value));
-                      //return true;
-                      return ($.inArray(n.type, event.value) > -1)
-                    },
-                    'test-filter')
-                    .apply();
-                }
-              });
-
-              //Call refresh to render the new graph
-              new_s.refresh();
-
-              // Finally, turn on force atlas 2
-              new_s.startForceAtlas2({
-                startingIterations: 150,
-                iterationsPerRender: 50,
-                barnesHutOptimize: false,
-                adjustSizes: true,
-                worker: true,
-                strongGravityMode: true,
-                lingLogMode: true
-              });
-
-              function stop() {
-                new_s.killForceAtlas2();
-              }
-
-              window.setTimeout(stop, 2000);
-              new_s.refresh();
-
-              // action if we click on a node
-              new_s.bind('clickNode', function(e) {
-                  window.console.log(e.type, e.data.node.label, e.data.captor);
-                  Shiny.onInputChange("comm_id", e.data.node.comm_id);
-              });
-              s = new_s;
-              
-              
-              
-              
-            }
-          );
-        }
-      );
-
-      // var g = document.querySelector('#graph2');
       var p = g.parentNode;
       p.removeChild(g);
       var c = document.createElement('div');
       c.setAttribute('id', 'graph2');
       p.appendChild(c);
-      
-      
-      
-      
-      
-//      $(document).on('shiny:inputchanged', function(event) {
-//        if (event.name === 'entTypes') {
-//          console.log(event.value);
-//        }
-//      });
 
+      //create_graph(); 
+
+      sigma.parsers.json("data/current_graph.json",
+        {
+          container: 'graph2'
+        },
+        function(s) {
+          
+          var defaultEntTypes = $("input[name='entTypes']:checked").map(function(){
+            return $(this).val();
+          });
+          console.log(defaultEntTypes.get());
+
+          s.graph.nodes().forEach(
+            function(node, i, a) {
+		          if(searchelm !== "") {
+			          for (var ix = 0; ix < searchelm.length; ix++) {
+				          elem = searchelm[ix];
+        					if (node.id == elem) { 
+                    node.color = "#FFD700";
+        					}
+        					else {
+        						node.color = node.originalcolor;
+        					} 
+                }
+              }
+            }
+          );
+          
+
+          var filter = new sigma.plugins.filter(s);
+          filter
+                .undo('node-filter')
+                .nodesBy(function(n) {
+                  return n.type=='Community' || (($.inArray(n.type, defaultEntTypes) > -1));
+                },
+                'node-filter')
+                .apply();
+
+          $(document).on('shiny:inputchanged', function(event) {
+            if (event.name === 'entTypes') {
+              filter
+                .undo('node-filter')
+                .nodesBy(function(n) {
+                  return n.type=='Community' || (($.inArray(n.type, event.value) > -1));
+                },
+                'node-filter')
+                .apply();
+            }
+          });
+
+
+          //Call refresh to render the new graph
+          s.refresh();
+
+          // Finally, turn on force atlas 2
+          s.startForceAtlas2({
+            startingIterations: 150,
+            iterationsPerRender: 50,
+            barnesHutOptimize: false,
+            adjustSizes: true,
+            worker: true,
+            strongGravityMode: true,
+            lingLogMode: true
+          });
+
+          function stop() {
+            s.killForceAtlas2();
+          }
+
+          window.setTimeout(stop, 2000);
+          s.refresh();
+
+          // action if we click on a node
+          s.bind('clickNode', function(e) {
+              window.console.log(e.type, e.data.node.label, e.data.captor);
+              Shiny.onInputChange("comm_id", e.data.node.comm_id);
+          });
+          
+          console.log("new commemmsg handler");
+          console.log(s.graph.nodes().length);
+          try {
+            Shiny.addCustomMessageHandler("commmemmsg",
+              function(message) {
+                console.log("commmemmsg");
+                JSON.parse(JSON.stringify(message), function(k, v) {
+                  if (k == "id") {
+                    var elems = String(v).split(",");
+                    console.log(s.graph.nodes().length);
+                    s.graph.nodes().forEach(
+                      function(node, i, a) {
+                        node.color = node.originalcolor;
+                        for (var ix = 0; ix < elems.length; ix++) {
+                          elem = elems[ix];
+                          if (node.id == elem) {
+                            node.color = "#FFD700";
+                          }
+                        }
+                        searchelm=elems;
+                      }
+                    );
+                    s.refresh();
+                  }
+                });
+              }
+            );
+          }
+          catch (err) {
+          }
+          
+        }
+      );
     }
   );
+  
+  
 });
